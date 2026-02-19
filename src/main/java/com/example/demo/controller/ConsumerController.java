bd214b4a45865827e7310f314dae0c115929e5e2
Anuj Gosavi <anujgosavi2005@gmail.com>
2026-02-20 00:16:55 +0530

    Added prometheus and kafka

--- src/main/java/com/example/demo/controller/ConsumerController.java
+++ src/main/java/com/example/demo/controller/ConsumerController.java
@@ -1,37 +1,24 @@
 package com.example.demo.controller;
 
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
 
-import java.util.Properties;
+@Service
+public class ConsumerController {
 
-@RestController
-@RequestMapping("/producer")
-public class ProducerController {
-    private static final String TOPIC_NAME = "testy";
-    private static final String BOOTSTRAP_SERVERS = "13.127.99.104:9092";
+    private final Counter kafkaEventsCounter;
 
-    private final Producer<String, String> kafkaProducer;
-
-    public ProducerController() {
-        Properties props = new Properties();
-        props.put("bootstrap.servers", BOOTSTRAP_SERVERS);
-        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
-        props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
-        this.kafkaProducer = new KafkaProducer<>(props);
+    public ConsumerController(MeterRegistry meterRegistry) {
+        this.kafkaEventsCounter = Counter.builder("kafka_events_received_total")
+                .description("Total number of Kafka events received")
+                .register(meterRegistry);
     }
 
-    @PostMapping("/event")
-    public void sendEventToKafka(@RequestBody String eventData) {
-        ProducerRecord<String, String> record = new ProducerRecord<>(TOPIC_NAME, "userEvent", eventData);
-        kafkaProducer.send(record, (metadata, exception) -> {
-            if (exception != null) {
-                System.err.println("Error sending message to Kafka: " + exception.getMessage());
-            } else {
-                System.out.println("Message sent to Kafka, offset: " + metadata.offset());
-            }
-        });
+    @KafkaListener(topics = "testy", groupId = "metrics-consumer-group")
+    public void listen(String eventData) {
+        System.out.println("Received event: " + eventData);
+        kafkaEventsCounter.increment();
     }
 }
